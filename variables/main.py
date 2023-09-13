a6c4217d696f6218ca14eddb8588be543a0eb768
keelan moore <HububPlaya>
2023-09-12 19:33:17 -0500

    variable practice

---
+++ variables/main.py
@@ -0,0 +1,17 @@
+#update the meu variable for each meal time 
+
+meal = "enlgish muffin"
+
+print("breaskfast: ")
+print(meal)
+
+meal = "grilled cheese and tomato soup"
+
+print("lunch: ")
+print(meal)
+
+meal = "steak and shrimp"
+
+print("dinner: ")
+print(meal)
+
