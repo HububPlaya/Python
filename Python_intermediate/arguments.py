ab489a317e511ad871026c374d759f3eb0553f00
Keelan Moore <135381527+HububPlaya@users.noreply.github.com>
2023-09-13 15:40:00 -0500

    unpacking arguments

--- Python_intermediate/arguments.py
+++ Python_intermediate/arguments.py
@@ -10,9 +10,37 @@ print_name("keelan", "moore")
 # Ex 2: keyword: this is where we can redefine our arguments 
 def print_birthday(set_month, day, year):
     print(set_month + day + year)
-print_name(set_month="August", day='13', year="2003") #this gave me a type error
+#print_name(set_month="August", day='13', year="2003") this gave me a type error
 
 # Ex 3: default: regular arguments that don'care about order or initlialization
-def print_name(firstName, lastName):
+"""def print_name(firstName, lastName):
     print(firstName + lastName)
-print_name()
+print_name()"""
+
+"""
+we can use the unpacking operator to use as many arguments as we want.
+the print method in python uses the unpacking operator to use multiple arguments 
+"""
+# Ex 1: unpacking arguments 
+def function_example(*args):
+    print(args)
+
+function_example("args", 12, False)
+
+# Ex 2: unpacking arguments effciently 
+def shout_strings(*args):
+    for argument in args:
+        print(argument.upper())
+
+shout_strings('Working on', 'learning', 'argument unpacking!')
+
+# ex 3: using mutiple arguments when unpacking 
+# make a function that shortens words to 8 characters 
+
+#length and loop through the args 
+# the colon with brackets means it creates a substring up to the but not including the length
+def truncate_sentence(length, *words):
+    for word in words:
+        print(word[:length])
+
+truncate_sentence(8, "I love the park", "I love school so much")
