5a5b10ca18b9eb44409735c874caca388e9573b8
Keelan Moore <135381527+HububPlaya@users.noreply.github.com>
2023-09-13 07:25:14 -0500

    updated arguments

--- Python_intermediate/arguments.py
+++ Python_intermediate/arguments.py
@@ -2,11 +2,17 @@
 We're going to cover three types of arguments: positional, keyword, and default
 """
 
-# Ex 1: positional
+# Ex 1: positional: uses the first and last name as the order (order matters)
 def print_name(firstName, lastName):
     print(firstName + lastName)
 print_name("keelan", "moore")
 
-# Ex 2: keyword
+# Ex 2: keyword: this is where we can redefine our arguments 
+def print_birthday(set_month, day, year):
+    print(set_month + day + year)
+print_name(set_month="August", day='13', year="2003") #this gave me a type error
 
-# Ex 3: default
+# Ex 3: default: regular arguments that don'care about order or initlialization
+def print_name(firstName, lastName):
+    print(firstName + lastName)
+print_name()
