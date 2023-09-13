3d3c28886240b63debb104b3d1ff43f0bf1e923f
Keelan Moore <135381527+HububPlaya@users.noreply.github.com>
2023-09-13 07:11:51 -0500

    arguments

---
+++ Python_intermediate/arguments.py
@@ -0,0 +1,12 @@
+"""
+We're going to cover three types of arguments: positional, keyword, and default
+"""
+
+# Ex 1: positional
+def print_name(firstName, lastName):
+    print(firstName + lastName)
+print_name("keelan", "moore")
+
+# Ex 2: keyword
+
+# Ex 3: default
