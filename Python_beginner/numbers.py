6d156893fee4b46f20fb48b6a1ea3bb9c823e983
Keelan Moore <135381527+HububPlaya@users.noreply.github.com>
2023-09-13 07:00:17 -0500

    added an operator

---
+++ Python_beginner/numbers.py
@@ -0,0 +1,38 @@
+# a number is can be an int (integer) or a float (decimal)
+
+# Ex 1:
+
+decimal = 1.5
+
+# Ex 2:
+
+whole_number = 11
+
+# you can use calculations with numbers 
+
+# Ex 1:
+# the calculation can produce either a float or int
+print(25 * 68 + 13 / 28) 
+
+# Ex 2:
+# calculation can update variables 
+
+quilt_width = 8
+quilt_length = 12
+
+# we update the length to 8
+quilt_length = 8
+
+# original output: 96
+# output: 64
+squares = quilt_width * quilt_length
+print("squares: " + squares)
+
+# Arithmetic Operators 
+
+#exponent 
+exponent = 6 ** 2
+
+#modulo takes the remainder from a division calculation 
+moduolo = 3 % 3 # ouputs 0 because 3 / 3 = 1 aka no remainder 
+
