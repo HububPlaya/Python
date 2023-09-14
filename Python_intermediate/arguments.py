59c59aed24ff088e89e272cfac43cb66808bbae9
Keelan Moore <135381527+HububPlaya@users.noreply.github.com>
2023-09-13 22:53:57 -0500

    coding problem reverse string

--- Python_intermediate/arguments.py
+++ Python_intermediate/arguments.py
@@ -34,8 +34,7 @@ def shout_strings(*args):
 
 shout_strings('Working on', 'learning', 'argument unpacking!')
 
-# ex 3: using mutiple arguments when unpacking 
-# make a function that shortens words to 8 characters 
+m# make a function that shortens words to 8 characters 
 
 #length and loop through the args 
 # the colon with brackets means it creates a substring up to the but not including the length
@@ -44,3 +43,59 @@ def truncate_sentence(length, *words):
         print(word[:length])
 
 truncate_sentence(8, "I love the park", "I love school so much")
+
+# Create a dictionary of tables 1 - 7 that holds the name, Vip_status, and order
+tables = {
+    1: {
+        'name': 'Keelan',
+        'vip_status': True,
+        'order': 'Orange Juice'
+    },
+    2: {},
+    3: {},
+    4: {},
+    5: {},
+    6: {},
+    7: {}
+}
+
+# Create a function to assign tables
+def assign_table(table_number, name, vip_status):
+
+    # Check if the table_number exists in the tables dictionary
+    if table_number in tables:
+        # Assign the customer's name to a specific table
+        tables[table_number]['name'] = name
+
+        # Assign the customer's vip_status to the specific table
+        tables[table_number]['vip_status'] = vip_status
+
+        # Assign the customer's order for the specific table
+        # It's initially blank so we can update it
+        tables[table_number]['order'] = ''
+    else:
+        print(f"Table {table_number} does not exist.")
+
+# Create a function that assigns and prints the order
+def assign_and_print_order(table_number, *order_items):
+
+    # Check if the table_number exists in the tables dictionary
+    if table_number in tables:
+        # Update the order attribute to hold multiple orders
+        tables[table_number]['order'] = order_items
+
+        # Loop and print each item from the multiple items
+        print(f"Order for Table {table_number}:")
+        for ordered_item in order_items:
+            print(ordered_item)
+    else:
+        print(f"Table {table_number} does not exist.")
+
+# Test 1
+assign_table(12, 'Keelan', True)
+assign_and_print_order(12, 'Lobster', 'Salmon', 'Salad')
+
+
+
+
+
